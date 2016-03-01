b6a293c3cf44ab5fce031787f7e7a42448360dbb
Patrick <p_donati@hotmail.com>
2016-03-01 12:07:43 +0000

    completed chapter 8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,3 +1,9 @@
-describe DockingStation do
+require "docking_station"
 
+describe DockingStation do
+  #it {is_expected.to respond_to(:release_bike)}
+
+  it 'responds to release_bike' do
+    expect(subject).to respond_to :release_bike
+  end  
 end
